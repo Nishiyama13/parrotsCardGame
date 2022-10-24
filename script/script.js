38803033b7f3f24e54fcd9be59fc8e6ec51a4367
Nishiyama13 <nishiyamaeel@gmail.com>
2022-10-24 14:53:43 -0300

    Versão funcional

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-//Global
+//Declaração das variáveis globais
 const imagesFront = [
   "./assets/image/araras.png",
   "./assets/image/araras_2.png",
@@ -8,25 +8,32 @@ const imagesFront = [
   "./assets/image/araras_6.png",
   "./assets/image/araras_7.png",
 ];
-let count = 0;
-
+let moves = 0;
+const list = [];
+let openCards = 0;
+let firstCard, secondCard;
+let numberCards = "";
+//Iníco do programa
+imputNumber();
 //Conferir numero de cartas
-let numberCards = Number(
-  prompt(
-    `Insira um número par entre 4 e 14 para escolher a quantidade de cartas:`
-  )
-);
+function imputNumber() {
+  numberCards = Number(
+    prompt(
+      `Insira um número par entre 4 e 14 para escolher a quantidade de cartas:`
+    )
+  );
 
-do {
-  if (numberCards % 2 !== 0 || numberCards < 4 || numberCards > 14) {
-    numberCards = prompt(
-      `ATENÇÃO! Informe um valor válido como 4, 6, 8, 10, 12 ou 14`
-    );
-  } else {
-    addCards();
-  }
-} while (numberCards % 2 !== 0 || numberCards < 4 || numberCards > 14);
-
+  do {
+    if (numberCards % 2 !== 0 || numberCards < 4 || numberCards > 14) {
+      numberCards = alert(
+        `ATENÇÃO! Informe um valor válido como 4, 6, 8, 10, 12 ou 14`
+      );
+      imputNumber();
+    } else {
+      addCards();
+    }
+  } while (numberCards % 2 !== 0 || numberCards < 4 || numberCards > 14);
+}
 //functions
 
 function addCards() {
@@ -57,22 +64,44 @@ function shuffleCards() {
   return Math.random() - 0.5;
 }
 
-//virar carta
+//virar cartas
 function turnCard(cardSelect) {
   cardSelect.classList.toggle("turn");
-  count++;
+  list.push(cardSelect);
+  moves++;
 
-  if (count == 2) {
-    count = 0;
-    // se as cartas forem diferentes
-    setTimeout(untapCard, 2000);
+  if (list.length === 2) {
+    setTimeout(comparator, 1000);
   }
 }
 
-//desvirar carta automaticamente
-function untapCard() {
-  const cards = document.querySelectorAll(".turn");
-  for (let i = 0; i < cards.length; i++) {
-    cards[i].classList.remove("turn");
+//Comparador
+function comparator() {
+  const firstCard = list[0].innerHTML;
+  const secondCard = list[1].innerHTML;
+
+  if (firstCard !== secondCard) {
+    while (list.length !== 0) {
+      list[0].classList.remove("turn");
+      list.shift();
+    }
+  }
+  if (firstCard === secondCard) {
+    openCards += 2;
+    console.log(openCards);
+    console.log(numberCards);
+    console.log(moves);
+    while (list.length !== 0) {
+      list.shift();
+    }
+
+    checkEndGame();
+  }
+}
+
+//Teste de fim de jogo
+function checkEndGame() {
+  if (openCards === numberCards) {
+    alert(`Você ganhou em ${moves} jogadas.`);
   }
 }
